89a71a3439f666ab94edf23c71fccd2ee3bab1b6
tnsoftbear <i.kotlevskis@bidpath.com>
2023-05-24 00:08:52 +0300

    Make CurrencyCheckbox as HookWidget (#5)

--- lib/feature/currency/internal/domain/model/currency.dart
+++ lib/feature/currency/internal/domain/model/currency.dart
@@ -36,23 +36,4 @@ final class Currency with EquatableMixin {
   }
 
   get props => [code, name, isVisibleForSource, isVisibleForTarget];
-
-  // @override
-  // bool operator ==(Object other) {
-  //   if (identical(this, other)) return true;
-  //
-  //   return other is Currency &&
-  //       other.code == code &&
-  //       other.name == name &&
-  //       other.isVisibleForSource == isVisibleForSource &&
-  //       other.isVisibleForTarget == isVisibleForTarget;
-  // }
-  //
-  // @override
-  // int get hashCode {
-  //   return code.hashCode ^
-  //       name.hashCode ^
-  //       isVisibleForSource.hashCode ^
-  //       isVisibleForTarget.hashCode;
-  // }
 }
