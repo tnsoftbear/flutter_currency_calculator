b64d395de27acc92bd52d3412e93e6927aee6dfb
tnsoftbear <i.kotlevskis@bidpath.com>
2023-05-23 10:00:39 +0300

    Make CurrencyCheckbox as HookWidget

--- lib/feature/currency/internal/ui/setting/currency_checkbox.dart
+++ lib/feature/currency/internal/ui/setting/currency_checkbox.dart
@@ -2,10 +2,11 @@ import 'package:currency_calc/feature/currency/internal/domain/model/currency.da
 import 'package:currency_calc/feature/currency/internal/domain/update/currency_visibility_updater.dart';
 import 'package:currency_calc/feature/setting/internal/domain/model/setting_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
-class CurrencyCheckbox extends StatefulWidget {
-  const CurrencyCheckbox(
+class CurrencyCheckbox extends HookWidget {
+  CurrencyCheckbox(
       this._currency, this._isSourceCurrency, this._currencyVisibilityUpdater,
       {Key? key})
       : super(key: key);
@@ -15,40 +16,26 @@ class CurrencyCheckbox extends StatefulWidget {
   final CurrencyVisibilityUpdater _currencyVisibilityUpdater;
 
   @override
-  _CurrencyCheckboxState createState() => _CurrencyCheckboxState();
-}
-
-class _CurrencyCheckboxState extends State<CurrencyCheckbox> {
-  late Currency _currency;
-
-  @override
-  void initState() {
-    super.initState();
-    _currency = widget._currency;
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final currencyNotifier = useState<Currency>(_currency);
     final settingModel = context.read<SettingModel>();
-    final key = widget._isSourceCurrency
+    final key = _isSourceCurrency
         ? 'sourceCurrency_${_currency.code}'
         : 'targetCurrency_${_currency.code}';
-    final onChange = widget._isSourceCurrency
-        ? widget._currencyVisibilityUpdater.changeVisibleSourceCurrency
-        : widget._currencyVisibilityUpdater.changeVisibleTargetCurrency;
+    final onChange = _isSourceCurrency
+        ? _currencyVisibilityUpdater.changeVisibleSourceCurrency
+        : _currencyVisibilityUpdater.changeVisibleTargetCurrency;
 
     return Checkbox(
       key: Key(key),
-      value: widget._isSourceCurrency
-          ? _currency.isVisibleForSource
-          : _currency.isVisibleForTarget,
+      value: _isSourceCurrency
+          ? currencyNotifier.value.isVisibleForSource
+          : currencyNotifier.value.isVisibleForTarget,
       onChanged: (bool? visible) async {
         Currency? updatedCurrency =
             await onChange(_currency.code, visible, settingModel);
         if (updatedCurrency != null) {
-          setState(() {
-            _currency = updatedCurrency;
-          });
+          currencyNotifier.value = updatedCurrency;
         }
       },
     );
